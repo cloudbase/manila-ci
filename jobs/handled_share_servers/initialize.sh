86ff63c9024c9fb1b1627e5c80734218afa27514
Andrei Bacos <andreibacos@gmail.com>
2017-05-17 19:38:45 +0300

    add missing quotes

--- jobs/handled_share_servers/initialize.sh
+++ jobs/handled_share_servers/initialize.sh
@@ -19,6 +19,6 @@ run_ssh_cmd_with_retry ubuntu@$FIXED_IP $DEVSTACK_SSH_KEY \
 if [[ "$ZUUL_BRANCH" == "master" ]] || [[ "$ZUUL_BRANCH" == "stable/ocata" ]]; then
     #run_ssh_cmd_with_retry ubuntu@$FIXED_IP $DEVSTACK_SSH_KEY \
     #    "url=\$(grep transport_url /etc/nova/nova-dhcpbridge.conf | awk '{print \$3}'); nova-manage cell_v2 simple_cell_setup --transport-url \$url >> /opt/stack/logs/screen/create_cell.log"
-    run_ssh_cmd_with_retry ubuntu@$FIXED_IP $DEVSTACK_SSH_KEY nova-manage cell_v2 discover_hosts --verbose
+    run_ssh_cmd_with_retry ubuntu@$FIXED_IP $DEVSTACK_SSH_KEY "nova-manage cell_v2 discover_hosts --verbose"
 fi
 
