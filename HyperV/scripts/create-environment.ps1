dc91331393e4b7ceccf80744dc22d6d0fc3ef96c
Andrei Bacos <andreibacos@gmail.com>
2017-06-26 21:13:35 +0300

    add transport_url variable

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -233,7 +233,13 @@ pip install amqp==2.1.3
 $cpu_array = ([array](gwmi -class Win32_Processor))
 $cores_count = $cpu_array.count * $cpu_array[0].NumberOfCores
 
-$novaConfig = (gc "$templateDir\nova.conf").replace('[DEVSTACK_IP]', "$devstackIP").Replace('[LOGDIR]', "$openstackLogs").Replace('[RABBITUSER]', $rabbitUser)
+if (($branchName -eq 'stable/newton') -or ($branchName -eq 'stable/ocata')) {
+    $celldb = "nova"
+} else {
+    $celldb = "nova_cell1"
+}
+
+$novaConfig = (gc "$templateDir\nova.conf").replace('[DEVSTACK_IP]', "$devstackIP").Replace('[LOGDIR]', "$openstackLogs").Replace('[RABBITUSER]', $rabbitUser).Replace('[CELLDB]', $celldb)
 $neutronConfig = (gc "$templateDir\neutron_hyperv_agent.conf").replace('[DEVSTACK_IP]', "$devstackIP").Replace('[LOGDIR]', "$openstackLogs").Replace('[RABBITUSER]', $rabbitUser).Replace('[CORES_COUNT]', "$cores_count")
 
 if (($branchName -ne 'stable/liberty') -and ($branchName -ne 'stable/mitaka')) {
